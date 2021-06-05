a372a5b6df0bfb1cdd7a0cbfb4732a9a3782f234
Dilana777 <dylana7792@hotmail.com>
2021-06-05 02:30:19 +0000

    guardando los primeros cambios

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,13 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import rigoImage from "../../img/hearts.png";
 import "../../styles/home.scss";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+		<h1>María Dilana Avendaño Chavarría</h1>
 		<p>
 			<img src={rigoImage} />
 		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<button href="#">Mira mis diseños</button>
 	</div>
 );
